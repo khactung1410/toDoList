96f51e90f3609cd0cc7d5911d9cab2d6df15051f
khactung1410 <duongkhactung16101997@gmail.com>
2019-02-11 10:23:33 +0700

    done basic redux

--- src/Training/actions/index.js
+++ src/Training/actions/index.js
@@ -1,13 +1,12 @@
 import * as type from '../constants/actionTypes'
 export const status = () =>{
     return {
-        type : type.TOGGLE_STATUS,
-
+        type : type.TOGGLE_STATUS
     }
 }
 export const sort = (sort) => {
     return {
         type : type.SORT,
-        sort : sort
+        sort
     }
 }
